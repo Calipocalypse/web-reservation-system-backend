151f56c8dee6b0ee57665dc7a383ad2752e790e6
Calipocalypse <calipocalypse@gmail.com>
2022-04-24 20:44:53 +0200

    Created User model and controller, set up route for it

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Wsr.Models;
+
+namespace Wsr.Controllers
+{
+    [ApiController]
+    [Route("[controller]" + "s")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public List<User> Get()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User("JohnyKAsteroid", "45hj23j523jnpsdgkpng", false));
+            return users;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Wsr.Models
+{
+    public class User
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public string HashedPassword { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User(string name, string hashedPassword, bool isAdmin)
+        {
+            Name = name;
+            HashedPassword = hashedPassword;
+            IsAdmin = isAdmin;
+        }
+    }
+}
